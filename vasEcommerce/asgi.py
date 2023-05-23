6ad2f5347f43aea156d76f44b42c7538f92c4336
Abdul Alim <aa.abdulalim13@gmail.com>
2023-05-23 15:27:21 +0600

    Resolve 'setup Django project'

---
+++ vasEcommerce/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vasEcommerce project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vasEcommerce.settings')
+
+application = get_asgi_application()
