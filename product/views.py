24bb6f645e3bc3bed64ee77ab624fe14dd21daaa
Abdul Alim <aa.abdulalim13@gmail.com>
2023-05-24 18:00:16 +0600

    VE-3 Resolve 'setup product app'

--- product/views.py
+++ product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def index(request):
+    product_list = Product.objects.all()
+    return render(request, 'product/index.html', {'products': product_list})
