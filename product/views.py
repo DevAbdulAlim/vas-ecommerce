5b93998f4b3edc3c64229d1010af795e70634863
Abdul Alim <aa.abdulalim13@gmail.com>
2023-05-25 15:29:45 +0600

    VE-13 Resolve 'create product detail view'

--- product/views.py
+++ product/views.py
@@ -5,3 +5,8 @@ from .models import Product
 def index(request):
     product_list = Product.objects.all()
     return render(request, 'product/products.html', {'products': product_list})
+
+def detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    product_list = Product.objects.filter(category = product.category)
+    return render(request, 'product/product-detail.html', {'product': product, 'products': product_list})
