4f2ba88f13c228aa5b114ef6e7818b892438a0e3
Abdul Alim <aa.abdulalim13@gmail.com>
2023-05-25 14:41:01 +0600

    VE-12 Resolve 'create a base template'

--- product/views.py
+++ product/views.py
@@ -4,4 +4,4 @@ from .models import Product
 # Create your views here.
 def index(request):
     product_list = Product.objects.all()
-    return render(request, 'product/index.html', {'products': product_list})
+    return render(request, 'product/products.html', {'products': product_list})
